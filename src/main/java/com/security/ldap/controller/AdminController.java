bca76721c3d4efadab96b4444496bb18971dd779
Ozan Koç <kocozannn@gmail.com>
2023-09-20 23:53:49 +0300

    ldap authentication DB authorization sample v2

--- src/main/java/com/security/ldap/controller/AdminController.java
+++ src/main/java/com/security/ldap/controller/AdminController.java
@@ -1,6 +1,5 @@
 package com.security.ldap.controller;
 
-import jakarta.annotation.security.RolesAllowed;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
